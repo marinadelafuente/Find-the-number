823a5a7346ea089fa4422574c0d9973ddebb66f0
marinadelafuente <mdelafuente18@gmail.com>
2019-11-20 10:15:43 +0100

    add js minus counter

--- js/main.js
+++ js/main.js
@@ -1,1 +1,43 @@
 'use-strict';
+
+const button = document.querySelector('.js-button');
+const userNumber = document.querySelector('.js-userNumber');
+const tipMessage = document.querySelector('.js-tipMessage');
+const counter = document.querySelector('.js-counter');
+
+const generateRandomNumber = function (max) {
+    console.log('Generando número aleatorio');
+    return Math.ceil(Math.random() * max);
+}
+
+const randomNumber = generateRandomNumber(100);
+
+const compareNumber = function () {
+
+    const userNumberValue = userNumber.value;
+    console.log(randomNumber);
+
+    if (userNumberValue > 100) {
+        console.log('El número debe estar entre 0 y 100.');
+        return tipMessage.innerHTML = 'El número debe estar entre 0 y 100.';
+    } else if (userNumberValue === randomNumber) {
+        console.log('Has ganado');
+        return tipMessage.innerHTML = 'Has ganado campeona!!!';
+    } else if (userNumberValue < randomNumber) {
+        console.log('Demasiado bajo.');
+        return tipMessage.innerHTML = `Demasiado bajo.`;
+    } else if (userNumberValue > randomNumber) {
+        console.log('Demasiado alto.');
+        return tipMessage.innerHTML = `Demasiado alto.`;
+    }
+}
+
+
+
+const handleEvent = function (ev) {
+    ev.preventDefault();
+    console.log('Previene el evento');
+    compareNumber();
+};
+
+button.addEventListener('click', handleEvent);
