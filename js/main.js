2833543220ed425d46a8aff45637a6aca19aab05
marinadelafuente <mdelafuente18@gmail.com>
2020-03-18 15:59:10 +0800

    add counter

--- js/main.js
+++ js/main.js
@@ -1,41 +1,87 @@
-'use-strict';
 
+const title = document.querySelector('.js-title');
 const button = document.querySelector('.js-button');
+const newGameButton = document.querySelector('.js-new-game-button');
+const form = document.querySelector('.js-form');
 const userNumber = document.querySelector('.js-userNumber');
 const tipMessage = document.querySelector('.js-tipMessage');
 const counter = document.querySelector('.js-counter');
+const gameLost = document.querySelector('.js-lost-game');
 
-const generateRandomNumber = function (max) {
-    console.log('Generating random number');
+const generateRandomNumber = (max) => {
     return Math.ceil(Math.random() * max);
 }
-
 const randomNumber = generateRandomNumber(100);
 
-const compareNumber = function () {
-
+const compareNumber = () => {
     const userNumberValue = parseInt(userNumber.value);
-    console.log(randomNumber);
 
-    if (userNumberValue > 100) {
+    if (userNumberValue > 100 || userNumberValue < 0) {
         console.log('The number must be between 0 and 100.');
-        return tipMessage.innerHTML = 'The number must be between 0 and 100.';
+        tipMessage.innerHTML = 'The number must be between 0 and 100.';
     } else if (userNumberValue === randomNumber) {
         console.log('You win');
-        return tipMessage.innerHTML = 'Oh, yeah. YOU WIN!!!';
+        tipMessage.innerHTML = 'Oh, yeah. YOU WIN!!!';
+        count();
     } else if (userNumberValue < randomNumber) {
         console.log('Too low.');
-        return tipMessage.innerHTML = `Too low...`;
+        tipMessage.innerHTML = `Too low...`;
+        count();
     } else if (userNumberValue > randomNumber) {
         console.log('Too high.');
-        return tipMessage.innerHTML = `Not that high, mate.`;
+        tipMessage.innerHTML = `Not that high, mate.`;
+        count();
     }
 }
 
-const handleEvent = function (ev) {
+let times = 0;
+
+const count = () => {
+    if (times === 3) {
+        gameLost.classList.remove('hidden');
+        title.classList.add('hidden');
+        tipMessage.innerHTML = `Sorry, the number was ${randomNumber}`;
+        counter.innerHTML = '';
+        button.classList.add('hidden');
+        newGameButton.classList.remove('hidden');
+        form.classList.add('hidden');
+    }
+    else if (userNumber !== randomNumber & times < 2) {
+        times++;
+        return counter.innerHTML = `You've tried ${times} times`;
+    }
+    else if ((times === 2)) {
+        times++;
+        return counter.innerHTML = `Only 1 attempt left`;
+    }
+    else {
+        counter.innerHTML = 'You have 4 attempts';
+    }
+}
+
+const newGame = () => {
+    times = 0;
+    gameLost.classList.add('hidden');
+    title.classList.remove('hidden');
+    button.classList.remove('hidden');
+    newGameButton.classList.add('hidden');
+    form.classList.remove('hidden');
+    userNumber.value = '';
+    counter.innerHTML = 'You have 4 attempts';
+    tipMessage.innerHTML = `Write a number between 0 and 100 <br> and hit the "try" button.`;
+    compareNumber();
+}
+
+const handleEvent = ev => {
     ev.preventDefault();
-    console.log('Prevents the event');
     compareNumber();
 };
 
+const handleNewGame = ev => {
+    ev.preventDefault();
+    newGame();
+}
+
 button.addEventListener('click', handleEvent);
+
+newGameButton.addEventListener('click', handleNewGame);
